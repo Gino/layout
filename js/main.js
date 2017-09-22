c09ef8d768e8190426225b6fe4b707f2e94fb022
Gino <ginolouwsma01@gmail.com>
2017-09-22 10:04:22 +0200

    Added responsiveness & added articles.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 window.onload = function () {
     CKEDITOR.replace( 'content' );
+
+    var d = new Date();
+    var date = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
+    document.getElementById("date").placeholder = date;
 };
